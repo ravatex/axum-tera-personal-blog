3eeec06cf4f33a59ff665113c372fedd8b7aa9f1
Kosmas Xenakis Karapanagiotis <k.xenakiskarapanagiotis@student.tudelft.nl>
2025-02-03 00:33:03 +0100

    added business inquiry to form made error showing to user

--- src/request.rs
+++ src/request.rs
@@ -1,4 +1,5 @@
 use axum::{http::StatusCode, response::IntoResponse};
+use axum::extract::Json;
 use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Serialize, Deserialize)]
@@ -6,9 +7,10 @@ pub struct Message {
     name: String,
     email: String,
     message: String,
+    is_business: bool,
 }
 
-pub async fn message_post(message: axum::extract::Json<Message>) -> impl IntoResponse {
+pub async fn message_post(Json(message): Json<Message>) -> impl IntoResponse {
     println!("{:?}", message);
     (StatusCode::OK, "Message recieved succesfully")
 }
